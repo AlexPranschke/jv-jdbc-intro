d6086aebde9f461e74fcbff1f43d937873201314
AlexPranschke <alexpranschke@gmail.com>
2026-09-20 15:43:22 +0200

    first

---
+++ src/main/java/mate/academy/dao/BookDao.java
@@ -0,0 +1,24 @@
+﻿package mate.academy.dao;
+
+import mate.academy.model.Book;
+
+import java.util.Optional;
+
+public interface Dao {
+    //crud
+    //create
+    void create(Book book);
+
+    //read
+    Book get(Long id);
+    Optional<Book> getById(Long id);
+
+    //update
+    void update(Book book);
+
+    //delete
+    void delete(Long id);
+
+
+
+}
